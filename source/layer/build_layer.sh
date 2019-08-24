bc17a13d3074fc6dd7482928dbf6506c8fde457e
Suherman <suherman@qantas.com.au>
2019-08-24 20:01:27 +1000

    Test skip building.

--- source/layer/build_layer.sh
+++ source/layer/build_layer.sh
@@ -12,25 +12,25 @@ mkdir -p ${WORKDIR}/projects
 cp php.ini ${WORKDIR}/projects
 cp bootstrap ${WORKDIR}/projects
 
-cd ${WORKDIR}
+# cd ${WORKDIR}
 
-curl -sL http://www.openssl.org/source/openssl-1.0.1k.tar.gz | tar -xvz
-cd openssl-1.0.1k && ./config && make && make install
+# curl -sL http://www.openssl.org/source/openssl-1.0.1k.tar.gz | tar -xvz
+# cd openssl-1.0.1k && ./config && make && make install
 
-cd ${WORKDIR}
+# cd ${WORKDIR}
 
-curl -sL https://github.com/php/php-src/archive/php-7.3.0.tar.gz | tar -xvz
-cd php-src-php-7.3.0 && ./buildconf --force && ./configure --prefix=${WORKDIR}/php-7-bin/ --with-openssl=/usr/local/ssl --with-curl --with-zlib --enable-zip --with-pdo-mysql --without-pear --enable-mbstring --enable-dom --enable-libxml --enable-tokenizer --enable-simplexml --enable-mysqlnd --enable-mysqlnd --enable-opcache --enable-json --enable-bcmath --enable-cli --enable-pcntl --enable-sockets --enable-static
+# curl -sL https://github.com/php/php-src/archive/php-7.3.0.tar.gz | tar -xvz
+# cd php-src-php-7.3.0 && ./buildconf --force && ./configure --prefix=${WORKDIR}/php-7-bin/ --with-openssl=/usr/local/ssl --with-curl --with-zlib --enable-zip --with-pdo-mysql --without-pear --enable-mbstring --enable-dom --enable-libxml --enable-tokenizer --enable-simplexml --enable-mysqlnd --enable-mysqlnd --enable-opcache --enable-json --enable-bcmath --enable-cli --enable-pcntl --enable-sockets --enable-static
 
-make install
+# make install
 
 mkdir -p ${WORKDIR}/projects/bin
 mkdir -p ${WORKDIR}/projects/lib
 
-cp -pR ${WORKDIR}/php-7-bin/bin/php ${WORKDIR}/projects/bin
-cp /usr/lib64/lib* ${WORKDIR}/projects/lib/
+# cp -pR ${WORKDIR}/php-7-bin/bin/php ${WORKDIR}/projects/bin
+ls -R /usr
 
-ls -R ${WORKDIR}/projects/lib/
+cp /usr/lib64/lib* ${WORKDIR}/projects/lib/
 
 ${WORKDIR}/projects/bin/php -v
 
